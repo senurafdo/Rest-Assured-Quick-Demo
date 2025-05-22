ba24ca008aa2c380a016c151af5a0cf780332e45
senurafdo <tnsenurafernando@gmail.com>
2025-05-22 14:36:02 +0200

    add services

--- src/main/java/com/restassured/service/app/AuthenticationService.java
+++ src/main/java/com/restassured/service/app/AuthenticationService.java
@@ -1,4 +1,4 @@
-package com.restassured.service;
+package com.restassured.service.app;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
--- src/main/java/com/restassured/service/app/BookingService.java
+++ src/main/java/com/restassured/service/app/BookingService.java
@@ -1,4 +1,22 @@
 package com.restassured.service.app;
 
+import com.restassured.util.RestClient;
+
+import static com.restassured.HttpMethod.GET;
+import static com.restassured.constant.ApplicationConstant.BOOKING_SERVICE_ENDPOINT;
+import static com.restassured.constant.ApplicationConstant.RESTFUL_BOOKER_BASE_URL;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.http.HttpStatus.SC_OK;
+
 public class BookingService {
+    public static String getBookingIdFromBookingDb() {
+        return new RestClient(RESTFUL_BOOKER_BASE_URL, BOOKING_SERVICE_ENDPOINT, EMPTY)
+                .sendRequest(GET)
+                .statusCode(SC_OK)
+                .extract()
+                .body()
+                .jsonPath()
+                .get("[0].bookingid")
+                .toString();
+    }
 }
